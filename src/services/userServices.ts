a8f767f3d06da19c8604067e6568a252f4896a32
CoviloMilos <miloscovilons@gmail.com>
2025-01-12 19:15:19 +0100

    Feed items improvements

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,5 +1,6 @@
 import { db } from "@/config/firebaseConfig"; // Assume you have a Firestore instance
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, documentId, getDoc, getDocs, query, where } from "firebase/firestore";
+import { CompanyInformation, IndividualInformation } from "../types/interfaces";
 
 export const getUserData = async (uid: string) => {
   const userDoc = doc(db, "users", uid);
@@ -9,4 +10,31 @@ export const getUserData = async (uid: string) => {
   } else {
     throw new Error("User not found");
   }
+};
+
+export const getUsersDataMapInChunks = async (
+  userIds: string[]
+): Promise<Record<string, IndividualInformation | CompanyInformation>> => {
+  const userChunks = [];
+  const chunkSize = 10;
+
+  for (let i = 0; i < userIds.length; i += chunkSize) {
+    userChunks.push(userIds.slice(i, i + chunkSize));
+  }
+
+  const userDocsPromises = userChunks.map((chunk) =>
+    getDocs(query(collection(db, "users"), where(documentId(), "in", chunk)))
+  );
+
+  const userDocsSnapshots = await Promise.all(userDocsPromises);
+  const userMap: Record<string, IndividualInformation | CompanyInformation> = {};
+
+  userDocsSnapshots.forEach((snapshot) => {
+    snapshot.forEach((userDoc) => {
+      const userData = userDoc.data();
+      userMap[userDoc.id] = { id: userDoc.id, ...userData } as unknown as IndividualInformation | CompanyInformation;
+    });
+  });
+
+  return userMap;
 };
