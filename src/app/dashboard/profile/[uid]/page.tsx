4dcba7c88b1862d1f8d811571a3a2afcf2fce4f9
stefanracic <stefracic@gmail.com>
2025-01-13 00:20:27 +0100

    Changes

--- src/app/dashboard/profile/[uid]/page.tsx
+++ src/app/dashboard/profile/[uid]/page.tsx
@@ -1,7 +1,11 @@
 import { Profile } from "@/components/profile/profile";
 
-const ProfilePage = async ({ params }: { params: string }) => {
-  const { uid } = await params;
+const ProfilePage = async ({
+  params,
+}: {
+  params: Promise<{ uid: string }>;
+}) => {
+  const { uid } = (await params) as { uid: string };
   return <Profile uid={uid} />;
 };
 
